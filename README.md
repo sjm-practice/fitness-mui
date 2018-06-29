c9c6e8d228b5db8c3d7f4fe71deef6c17653ec37
stevenjmarsh <sjm.atl@gmail.com>
2018-06-28 23:14:19 -0400

    creating initial meteor project

---
+++ README.md
@@ -0,0 +1,2 @@
+# fitness-mui
+A simple project building a fitness application, for practice with Material-UI.
---
+++ imports/ui/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+
+// App component - represents the whole app
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
+  }
+}
