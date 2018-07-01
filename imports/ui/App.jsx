905bceff679ad105e082f332be662f4bfa9db259
stevenjmarsh <sjm.atl@gmail.com>
2018-07-01 17:20:59 -0400

    clearing eslint errors

---
+++ imports/ui/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+
+// App component - represents the whole app
+const App = () => (
+  <div>
+    <NavBar />
+  </div>
+);
+
+export default App;
